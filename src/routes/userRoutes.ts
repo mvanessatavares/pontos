b3ce6097a7cfeb2ae70b6fb05a794fcb57ac2f20
Vanessa Lira <vanessa.maria@academico.ifpb.edu.br>
2023-06-11 21:04:28 -0300

    Update userRoutes.ts

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -2,13 +2,32 @@ import { Router } from "express";
 import { CreateUserController } from "../controller/CreateUserController";
 import { SessionController } from "../controller/SessionController";
 import { CreatPointController} from "../controller/CreatPointController";
+//import {listarPontos} from "../controller/teste"
+
+const router = Router()
+const { PrismaClient } = require('@prisma/client')
+
+const prisma = new PrismaClient()
 
 const routes = Router();
 
 routes.post("/login", new SessionController().handle);
 routes.post("/users", new CreateUserController().handle);
-routes.post("/points", new CreatPointController ().handle )
+routes.post("/points", new CreatPointController().handle );
+router.get("/points", async (req, res) => {
+    try {
+      const pontos = await prisma.point.findMany()
+      res.json({ pontos })
+    } catch (err) {
+      console.error(err)
+      res.status(500).json({ error: 'Erro ao listar pontos' })
+    } finally {
+      await prisma.$disconnect()
+    }
+  })
 
 
-export { routes };
+
+export { routes, router };
+
 

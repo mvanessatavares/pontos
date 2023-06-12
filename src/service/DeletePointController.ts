498885581465c74502f42dec0f653cb46a36ebcc
Vanessa Lira <vanessa.maria@academico.ifpb.edu.br>
2023-06-12 20:06:20 -0300

    Add files via upload

---
+++ src/service/DeletePointController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+import { DeletePointService } from "../../service/pointsServer/DeletePointService";
+
+export class DeletePointController {
+  async handle(req: Request, res: Response) {
+    const  name  = req.params.name as string;
+    console.log(name);
+
+    const deletePointService = new DeletePointService();
+    const result = await deletePointService.execute( name );
+
+    if (result instanceof Error) {
+      return res.status(400).json(result.message);
+    }
+
+    return res.json(result);
+  }
+}

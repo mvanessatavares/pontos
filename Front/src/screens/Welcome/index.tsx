3cf6032a2f31b9bb98f39e1da4500dbb8b6d1bb9
Vanessa Lira <vanessa.maria@academico.ifpb.edu.br>
2023-06-26 21:44:09 -0300

    Update index.tsx

--- Front/src/screens/Welcome/index.tsx
+++ Front/src/screens/Welcome/index.tsx
@@ -2,10 +2,11 @@ import { Container } from './styles';
 import React from "react";
 import { ImageBackground, Image, Text, View, TouchableOpacity, SafeAreaView } from "react-native";
 import { styles } from "../Welcome/styles";
-
-
+import { Home } from "../Home"
+import { useNavigation } from '@react-navigation/native';
 
 export const Welcome = () => {
+  const navigation = useNavigation()
   return (
       <View style ={styles.container}>
       <ImageBackground source={require('../../../assets/fundo.png')}  style={styles.imageBackground}>
@@ -13,7 +14,8 @@ export const Welcome = () => {
         <View>
             <Text style = {styles.text}>Sejam bem vindos ao ReciclAE {'\n'} Seu app de reciclagem!</Text></View>
             <View style={styles.containerr}>
-            <TouchableOpacity style = {styles.button}>
+            <TouchableOpacity style = {styles.button}
+            onPress={() => navigation.navigate('Home')}>
                 <Text style = {styles.buttonText}>Acessar</Text>
             </TouchableOpacity>
             </View>

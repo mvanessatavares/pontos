2c227b36e4a1dbe6e9d5361d3646755a27a4d5f0
Vanessa Lira <vanessa.maria@academico.ifpb.edu.br>
2023-07-11 11:05:58 -0300

    Update index.tsx

--- Front/src/screens/Profile/index.tsx
+++ Front/src/screens/Profile/index.tsx
@@ -27,6 +27,27 @@ const dadosUsuario = {
 
 
 export const Profile = () => {
+
+  //a imagem de exemplo foi add por url
+
+  const [image, setImage] = useState('https://th.bing.com/th/id/R.a4ac01af50f9ee6fcf5635464b76fb92?rik=LXcNrNWjxAXZAA&riu=http%3a%2f%2fwww.apte-autisme.net%2fwp-content%2fuploads%2f2017%2f02%2fPicto-Professeur.png&ehk=Xo2Hk1tgCACsW67wYF6v%2b65Veoo9nTU6kPOZi%2bNTOCA%3d&risl=&pid=ImgRaw&r=0');
+
+  
+  const handleImagePicker = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      aspect: [4, 4],
+      allowsEditing: true,
+      base64: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+  function renderItem({ item }: ListRenderItemInfo<Point>) {
+    return <CollectPoint {...item} />;
+  }
   function renderItem({ item }: ListRenderItemInfo<Point>) {
     return <CollectPoint {...item} />;
   }
@@ -36,12 +57,25 @@ export const Profile = () => {
       <ImageBackground source={require('../../../assets/fundo.png')}  style={styles.image}>
 
     <View style={styles.userBox}>
+
+//eu apaguei toda essa parte de Image
+      
     <View style={styles.ima} >
 
        
                     <Image source={perfil}
                     style={styles.Image}  />
       </View>
+
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Image
+        source={{ uri: image }}
+        style={{ width: 100, height: 100, borderRadius: 50, marginTop: 10 }}
+      />
+      <TouchableOpacity onPress={handleImagePicker} style={{ marginTop: 20 }}>
+        <Text style = {{ color: '#fff' }}> Alterar foto de perfil </Text>
+      </TouchableOpacity>
+    </View>
      
       <Text style={styles.textUser}>{dadosUsuario.nome}</Text>
       <Text style={styles.textUser}>{dadosUsuario.email}</Text>
